1f65fbd186b9389131e11e88065b623191a18d63
JacopoDaeli <jacopo.daeli@gmail.com>
2016-06-24 14:22:17 +0100

    add intent-group

--- src/dialog/intent-dialog.js
+++ src/dialog/intent-dialog.js
@@ -2,20 +2,48 @@
 
 import Dialog from './dialog'
 
-export default class IntentDialog extends Dialog {
+class IntentDialog extends Dialog {
   constructor () {
     super()
+    this.groups = {}
+    this.confidenceThreshold = 0.1
+    this.intentThreshold = 0.1 // TODO: replace this with the above one
   }
 
   begin (session, args) {
-
+    if (this.beginDialog) {
+      this.beginDialog(session, args, () => {
+        super.begin.call(this, session, args)
+      })
+    } else {
+      super.begin.call(this, session, args)
+    }
   }
 
   replyReceived (session) {
-
+    const msg = session.message
+    this.recognizeIntents(msg.language, msg.text, (err, intents, entities) => {
+      if (!err) {
+        const topIntent = this.findTopIntent(intents)
+        const score = topIntent ? topIntent.score : 0
+        session.compareConfidence(msg.language, msg.text, score, (handled) => {
+          if (!handled) {
+            this.invokeIntent(session, intents, entities)
+          }
+        })
+      } else {
+        session.endDialog({
+          error: new Error(`Intent recognition error: ${err.message}`)
+        })
+      }
+    })
   }
 
   dialogResumed (session, result) {
 
   }
 }
+
+IntentDialog.CAPTURE_THRESHOLD = 0.6
+
+export default IntentDialog
---
+++ src/dialog/intent-group.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import actions from './DialogAction'
+
+export default class IntentGroup {
+  constructor (id) {
+    this.id = id
+    this.handlers = {}
+  }
+
+  _intentHandler (intent) {
+    return this.handlers[intent]
+  }
+
+  getId () {
+    return this.id
+  }
+
+  on (intent, dialogId, dialogArgs) {
+    if (!this.handlers.hasOwnProperty(intent)) {
+      if (Array.isArray(dialogId)) {
+        this.handlers[intent] = actions.waterfall(dialogId)
+      } else if (typeof dialogId === 'string') {
+        this.handlers[intent] = actions.DialogAction.beginDialog(dialogId, dialogArgs)
+      } else {
+        this.handlers[intent] = actions.waterfall([dialogId])
+      }
+    } else {
+      throw new Error(`Intent[${intent}] already exists.`)
+    }
+    return this
+  }
+}
