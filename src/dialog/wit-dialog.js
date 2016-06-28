e1f41f7282cd35bcfe858aedbbd6d25991d1bdbe
JacopoDaeli <jacopo.daeli@gmail.com>
2016-06-28 15:14:34 +0100

    clean wit-dialog.js file

--- src/dialog/wit-dialog.js
+++ src/dialog/wit-dialog.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import request from 'request'
+// import request from 'request'
 import IntentDialog from './intent-dialog'
 
 export class WitDialog extends IntentDialog {
@@ -10,32 +10,6 @@ export class WitDialog extends IntentDialog {
   }
 
   recognizeIntents (language, utterance, callback) {
-    let uri = this.serviceUri.trim()
-    if (uri.lastIndexOf('&q=') !== uri.length - 3) {
-      uri += '&q='
-    }
-    uri += encodeURIComponent(utterance || '')
-    request.get(uri, (err, res, body) => {
-      let calledCallback = false
-      try {
-        if (!err) {
-          const result = JSON.parse(body)
-          if (result.intents.length === 1 && typeof result.intents[0].score !== 'number') {
-            result.intents[0].score = 1.0
-          }
-          calledCallback = true
-          callback(null, result.intents, result.entities)
-        } else {
-          calledCallback = true
-          callback(err)
-        }
-      } catch (e) {
-        if (!calledCallback) {
-          callback(e)
-        } else {
-          console.error(e.toString())
-        }
-      }
-    })
+
   }
 }
